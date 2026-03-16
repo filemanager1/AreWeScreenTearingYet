9f75d4fe51f97c6c1986629a749f4e6a77b4c840
Matthew Trejo <matthew.trejo@refraction.software>
2026-03-16 16:10:49 -0500

    Update favicon for a proper one

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,8 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Are We Screen Tearing Yet?</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
       <Component {...pageProps} />
     </MantineProvider>
